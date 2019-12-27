61c10a6b06cf8275e0ad665f3e56c7744d5a25d4
Sajjad Dehghani <sademakn@gmail.com>
2019-12-27 19:44:29 +0330

    iframe has been fixed

--- jobvision/crawler.py
+++ jobvision/crawler.py
@@ -9,16 +9,17 @@ chrome_options.add_argument("--maximize")
 
 
 def get_jobvision_jobs(job_query):
-    # debug mode :
-    driver = webdriver.Chrome(
-        executable_path="driver_files/chromedriver", options=chrome_options
-    )
-
-    # ## production mode :
-    # driver = webdriver.Remote(
-    #     command_executor="http://selenium:4444/wd/hub",
-    #     desired_capabilities=webdriver.common.desired_capabilities.DesiredCapabilities.CHROME,
+    # job_query = "منشی"
+    # # debug mode :
+    # driver = webdriver.Chrome(
+    #     executable_path="driver_files/chromedriver", options=chrome_options
     # )
+
+    ## production mode :
+    driver = webdriver.Remote(
+        command_executor="http://selenium:4444/wd/hub",
+        desired_capabilities=webdriver.common.desired_capabilities.DesiredCapabilities.CHROME,
+    )
 
     driver.get(url)
 
@@ -34,12 +35,19 @@ def get_jobvision_jobs(job_query):
 
     job_post_search_sender = driver.find_element_by_id("job-post-search-sender")
     job_post_search_sender.click()
-    print(">>>> btn clicked !!!!!!!!!!!!!!!!!!!!")
+
     try:
-        driver.switch_to_default_content()
+        driver.switch_to.default_content()
     except:
         pass
-    # TODO: fix iframe
+    el = driver.find_element_by_class_name("Hide-screen")
+
+    try:
+        el.screen_shot("k.png")
+    except:
+        print("error")
+        sleep(3)
+
     jobs = []
     for jobpost in driver.find_elements_by_class_name("jobpost-box"):
         job_link = Base_url + jobpost.get_attribute("data-href")
